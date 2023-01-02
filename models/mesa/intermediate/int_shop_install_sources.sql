685a7106a7cdd72de5983c0d9bc8fdb347031d67
Jonathan Crawford <mail@joncrawford.com>
2023-01-02 13:31:07 -0800

    Refactor to diagnose and then remove blank space referrers. Use fancier source/medium names. Remove referrers after first install.

--- models/mesa/intermediate/int_shop_install_sources.sql
+++ models/mesa/intermediate/int_shop_install_sources.sql
@@ -1,4 +1,13 @@
+{#- cSpell:words INITCAP -#}
+
 WITH
+shops AS (
+    SELECT
+        shop_subdomain,
+        first_installed_at_pt
+    FROM {{ ref('stg_shops') }}
+),
+
 install_page_sessions AS (
 
     SELECT
@@ -9,66 +18,110 @@ install_page_sessions AS (
 
 ),
 
-formatted_install_pageviews AS (
-
+segment_web_sessions AS (
     SELECT
-        blended_user_id AS shop_subdomain,
-        utm_campaign AS acquisition_campaign,
-        utm_content AS acquisition_content,
-        referrer AS acquisition_referrer,
-        first_page_url_path AS acquisition_first_page_path,
-        CASE
-            WHEN referrer ILIKE '%apps.shopify.com'
-                THEN 'Shopify App Store'
-            ELSE COALESCE(referrer_source, utm_source)
-        END AS acquisition_source,
-        COALESCE(referrer_medium, utm_medium) AS acquisition_medium,
-        acquisition_source || ' - ' || acquisition_medium AS acquisition_source_medium,
-        tstamp
-    FROM install_page_sessions
-    LEFT JOIN {{ ref('segment_web_sessions') }} USING (session_id)
-    WHERE acquisition_source IS NOT NULL
-
+        * EXCLUDE (blended_user_id),
+        blended_user_id AS shop_subdomain
+    FROM {{ ref('segment_web_sessions') }}
 ),
 
 raw_install_events AS (
-    SELECT * FROM {{ source('mongo_sync', 'mesa_install_events') }}
+    SELECT
+        * EXCLUDE (uuid),
+        uuid AS shop_subdomain
+    FROM {{ source('mongo_sync', 'mesa_install_events') }}
+),
+
+formatted_install_pageviews AS (
+    {# TODO: Don't count a session after first-install #}
+    SELECT
+        shop_subdomain,
+        {{ pacific_timestamp('tstamp') }} AS tstamp_pt,
+        utm_content AS acquisition_content,
+        first_page_url_path AS acquisition_first_page_path,
+        utm_campaign AS acquisition_campaign,
+        referrer AS acquisition_referrer,
+        referrer_host,
+        CASE
+            WHEN referrer ILIKE '%apps.shopify.com%'
+                THEN 'Shopify App Store'
+            ELSE COALESCE(referrer_source, utm_source)
+        END AS acquisition_source,
+        COALESCE(referrer_medium, utm_medium) AS acquisition_medium
+    FROM install_page_sessions
+    LEFT JOIN segment_web_sessions USING (session_id)
+    LEFT JOIN shops USING (shop_subdomain)
+    WHERE acquisition_source IS NOT NULL
+        AND tstamp_pt <= first_installed_at_pt + INTERVAL '60seconds'
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY shop_subdomain ORDER BY tstamp_pt ASC) = 1
 ),
 
 formatted_install_events AS (
     SELECT
-        uuid AS shop_subdomain,
-        utm_campaign AS acquisition_campaign,
+        shop_subdomain,
+        {{ pacific_timestamp('created_at') }} AS tstamp_pt,
+        template AS acquisition_template,
         NULL AS acquisition_content,
-        referer AS acquisition_referrer,
         NULL AS acquisition_first_page_path,
+        NULLIF(utm_campaign, '') AS acquisition_campaign,
+        NULLIF(referer, '') AS acquisition_referrer,
+        NULLIF(utm_medium, '') AS acquisition_medium,
         CASE
-            WHEN referer ILIKE '%apps.shopify.com'
+            WHEN (referer ILIKE '%apps.shopify.com%')
                 THEN 'Shopify App Store'
-            ELSE COALESCE(referer, utm_source)
-        END AS acquisition_source,
-        utm_medium AS acquisition_medium,
-        acquisition_source || ' - ' || acquisition_medium AS acquisition_source_medium,
-        created_at AS tstamp
+            ELSE NULLIF(COALESCE(utm_source, referer), '')
+        END AS acquisition_source
     FROM raw_install_events
+    LEFT JOIN shops USING (shop_subdomain)
+    WHERE tstamp_pt <= first_installed_at_pt + INTERVAL '60seconds'
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY shop_subdomain ORDER BY tstamp_pt ASC) = 1
+),
+
+combined_install_sources AS (
+    SELECT
+        shop_subdomain,
+        acquisition_template,
+        referrer_host,
+        COALESCE(formatted_install_pageviews.tstamp_pt, formatted_install_events.tstamp_pt) AS tstamp_pt,
+        COALESCE(formatted_install_pageviews.acquisition_campaign, formatted_install_events.acquisition_campaign) AS acquisition_campaign,
+        COALESCE(formatted_install_pageviews.acquisition_content, formatted_install_events.acquisition_content) AS acquisition_content,
+        COALESCE(formatted_install_pageviews.acquisition_referrer, formatted_install_events.acquisition_referrer) AS acquisition_referrer,
+        COALESCE(formatted_install_pageviews.acquisition_first_page_path, formatted_install_events.acquisition_first_page_path) AS acquisition_first_page_path,
+        COALESCE(formatted_install_pageviews.acquisition_source, formatted_install_events.acquisition_source) AS acquisition_source,
+        COALESCE(formatted_install_pageviews.acquisition_medium, formatted_install_events.acquisition_medium) AS acquisition_medium
+    FROM formatted_install_pageviews
+    FULL OUTER JOIN formatted_install_events USING (shop_subdomain)
+),
+
+referrer_mapping as (
+    SELECT * FROM {{ ref('referrer_mapping') }}
 ),
 
 final AS (
     SELECT
         shop_subdomain,
-        acquisition_campaign,
-        acquisition_content,
         acquisition_referrer,
+        acquisition_template,
+        acquisition_content,
         acquisition_first_page_path,
-        acquisition_source,
-        acquisition_medium,
-        acquisition_source_medium
-    FROM (
-        SELECT * FROM formatted_install_events
-        UNION ALL
-        SELECT * FROM formatted_install_pageviews
-    )
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY shop_subdomain ORDER BY tstamp ASC) = 1
+        INITCAP(REPLACE(acquisition_campaign, '_', ' ')) AS acquisition_campaign,
+        INITCAP(REPLACE(COALESCE(referrer_mapping.medium, acquisition_medium), '_', ' ')) AS acquisition_medium,
+        INITCAP(COALESCE(referrer_mapping.source, acquisition_source)) AS acquisition_source,
+        INITCAP(
+            NULLIF(
+                (
+                    COALESCE(referrer_mapping.source, acquisition_source)
+                    || ' - '
+                    || COALESCE(referrer_mapping.medium, acquisition_medium)
+                ),
+                ' - '
+            )
+        ) AS acquisition_source_medium,
+        COALESCE(acquisition_first_page_path ILIKE '/blog/%', FALSE) AS is_blog_referral
+    FROM combined_install_sources
+    LEFT JOIN referrer_mapping
+        ON combined_install_sources.referrer_host = referrer_mapping.host
 )
 
-SELECT * FROM final
+SELECT *
+FROM final
