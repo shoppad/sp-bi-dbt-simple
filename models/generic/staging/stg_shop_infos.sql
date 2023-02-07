f1b1cf5bfd062e6cfa7f9d96611dd1df36948f21
Jonathan Crawford <mail@joncrawford.com>
2023-02-07 12:09:09 -0600

    Convert Shop Leads to Numerics and add better docs.

--- models/generic/staging/stg_shop_infos.sql
+++ models/generic/staging/stg_shop_infos.sql
@@ -14,11 +14,13 @@ WITH flattened_table AS (
 
 SELECT
     shop_subdomain,
-    categories[0]::STRING AS shop_category,
+    categories[0]::STRING AS store_leads_category,
+    SPLIT_PART(store_leads_category, '/', 2) AS store_leads_top_level_category,
+
     features AS shop_features,
-    COALESCE(LOWER(ARRAY_TO_STRING(features, '')) ILIKE '%recharge%', FALSE) AS has_recharge,
-    platform_rank AS shop_platform_rank,
-    platform_rank_percentile AS shop_platform_rank_percentile,
-    estimated_sales AS shop_estimated_sales,
-    monthly_app_spend AS shop_monthly_app_spend
+    COALESCE(LOWER(ARRAY_TO_STRING(features, '')) ILIKE '%recharge%', FALSE) AS store_leads_has_recharge,
+    platform_rank::NUMERIC AS store_leads_platform_rank,
+    platform_rank_percentile::NUMERIC AS store_leads_platform_rank_percentile,
+    estimated_sales::NUMERIC AS store_leads_estimated_monthly_sales,
+    monthly_app_spend::NUMERIC AS store_leads_monthly_app_spend
 FROM flattened_table
