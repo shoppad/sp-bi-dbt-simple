e6162eac2e190676c932a127ba5d2948f5b954b2
Jonathan Crawford <mail@joncrawford.com>
2023-05-10 12:13:05 -0700

    Fold in new Workflow wizard completion numbers

--- models/mesa/mart/rollups/mesa_shop_days.sql
+++ models/mesa/mart/rollups/mesa_shop_days.sql
@@ -7,11 +7,23 @@ shop_days AS (
 
 ),
 
+workflow_creations AS (
+    SELECT
+        shop_subdomain,
+        date_trunc('day', created_at_pt) AS dt,
+        COUNT(workflows.*) AS workflows_created_count,
+        COUNT_IF(workflows.setup IN ('incomplete', 'complete')) AS workflows_wizard_started_count,
+        COUNT_IF(workflows.setup = 'complete') AS workflows_wizard_complete_count,
+        COUNT_IF(workflows.setup = 'incomplete') AS workflows_wizard_incomplete_count
+    FROM {{ ref('workflows') }}
+    GROUP BY 1, 2
+),
+
 final AS (
 
     SELECT *
     FROM shop_days
-
+    LEFT JOIN workflow_creations USING (shop_subdomain, dt)
 )
 
 SELECT * FROM final
