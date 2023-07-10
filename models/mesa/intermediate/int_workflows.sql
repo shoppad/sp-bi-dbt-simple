6b60c725b35adbd13538bc81e37ab06c6a7b5d66
Jonathan Crawford <mail@joncrawford.com>
2023-07-10 12:15:44 -0700

    Forgot new file

---
+++ models/mesa/intermediate/int_workflows.sql
@@ -0,0 +1,33 @@
+WITH workflows AS (
+
+    SELECT *
+    FROM {{ ref('stg_workflows') }}
+
+),
+workflow_steps AS (
+
+    SELECT *
+    FROM {{ ref('stg_workflow_steps') }}
+    WHERE NOT is_deleted
+
+),
+
+app_chains AS (
+
+    SELECT
+        workflow_id,
+        LISTAGG(integration_app, ' • ') WITHIN GROUP (ORDER BY position_in_workflow ASC) AS app_chain,
+        LISTAGG(CONCAT(integration_app, ' →  ' , step_name), ' • ') WITHIN GROUP (ORDER BY position_in_workflow ASC) AS step_chain
+    FROM workflow_steps
+    GROUP BY 1
+
+),
+
+final AS (
+    SELECT *
+    FROM workflows
+    LEFT JOIN app_chains USING (workflow_id)
+)
+
+SELECT *
+FROM final
