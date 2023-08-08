39df8e8ea7da7b1df991080a3193662da9261084
Jonathan Crawford <mail@joncrawford.com>
2023-08-08 10:59:23 -0500

    Pull in deprecated CIO tables for 2 models

--- models/customer_io/staging/stg_email_clicks.sql
+++ models/customer_io/staging/stg_email_clicks.sql
@@ -16,24 +16,72 @@ newsletters AS (
 ),
 
 raw_email_clicks AS (
+    SELECT *
+    FROM {{ source('customer_io', 'email_clicks') }}
+),
+
+raw_email_clicks_deprecated AS (
+    SELECT *
+    FROM {{ source('customer_io_deprecated', 'email_clicks_deprecated') }}
+),
+
+decorated_email_clicks AS (
     SELECT
-        * EXCLUDE (
-                __hevo_id,
-                __hevo__ingested_at,
-                __hevo__loaded_at
-            )
-            RENAME (
-                customer_id AS shop_subdomain
-            ),
+        action_id,
+        campaign_id,
+        customer_id AS shop_subdomain,
+        delivery_id,
+        href,
+        journey_id,
+        link_id,
+        recipient,
+        subject,
+        content_id,
+        newsletter_id,
+        parent_action_id,
+        trigger_event_id,
         to_timestamp(__hevo__ingested_at::STRING) AS clicked_at_utc,
         {{ pacific_timestamp('clicked_at_utc') }} AS clicked_at_pt,
         IFF(journey_id IS NULL, 'broadcast', 'journey') AS email_type,
         IFF(journey_id IS NOT NULL, 'campaign-' || campaign_id || '-action-' || action_id, 'broadcast-' || newsletter_id) AS email_id
-    FROM {{ source('customer_io', 'email_clicks') }}
+    FROM raw_email_clicks
+),
+
+decorated_email_clicks_deprecated AS (
+    SELECT
+        action_id,
+        campaign_id,
+        customer_id AS shop_subdomain,
+        delivery_id,
+        href,
+        journey_id,
+        link_id,
+        recipient,
+        subject,
+        content_id,
+        newsletter_id,
+        parent_action_id,
+        NULL AS trigger_event_id,
+        to_timestamp(sent_at::STRING) AS clicked_at_utc,
+        {{ pacific_timestamp('clicked_at_utc') }} AS clicked_at_pt,
+        IFF(journey_id IS NULL, 'broadcast', 'journey') AS email_type,
+        IFF(journey_id IS NOT NULL, 'campaign-' || campaign_id || '-action-' || action_id, 'broadcast-' || newsletter_id) AS email_id
+    FROM raw_email_clicks_deprecated
+),
+
+combined_email_clicks AS (
+
+    SELECT
+        *
+    FROM decorated_email_clicks
+    UNION ALL
+    SELECT
+        *
+    FROM decorated_email_clicks_deprecated
 )
 
 SELECT *
-FROM raw_email_clicks
+FROM combined_email_clicks
 LEFT JOIN campaigns USING (campaign_id)
 LEFT JOIN newsletters USING (newsletter_id)
 WHERE campaign_id IN (SELECT campaign_id FROM campaigns)
--- models/customer_io/staging/stg_email_conversions.sql
+++ models/customer_io/staging/stg_email_conversions.sql
@@ -4,6 +4,11 @@ raw_email_conversions AS (
     FROM {{ source('customer_io', 'email_conversions') }}
 ),
 
+raw_email_conversions_deprecated AS (
+    SELECT *
+    FROM {{ source('customer_io_deprecated', 'email_conversions_deprecated') }}
+),
+
 campaigns AS (
     SELECT
         campaign_id,
@@ -23,16 +28,19 @@ newsletters AS (
 ),
 
 decorated_email_conversions AS (
+
     SELECT
-        *
-            EXCLUDE (
-                __hevo_id,
-                __hevo__ingested_at,
-                __hevo__loaded_at
-            )
-            RENAME (
-                customer_id AS shop_subdomain
-            ),
+        action_id,
+        campaign_id,
+        customer_id AS shop_subdomain,
+        delivery_id,
+        journey_id,
+        recipient,
+        subject,
+        content_id,
+        newsletter_id,
+        trigger_event_id,
+        parent_action_id,
         to_timestamp(__hevo__ingested_at::STRING) AS converted_at_utc,
         {{ pacific_timestamp('converted_at_utc') }} AS converted_at_pt,
         IFF(journey_id IS NULL, 'broadcast', 'journey') AS email_type,
@@ -43,12 +51,49 @@ decorated_email_conversions AS (
                 'broadcast-' || newsletter_id
             END AS email_id
     FROM raw_email_conversions
+),
+
+decorated_email_conversions_deprecated AS (
+
+    SELECT
+        action_id,
+        campaign_id,
+        customer_id AS shop_subdomain,
+        delivery_id,
+        journey_id,
+        recipient,
+        subject,
+        content_id,
+        newsletter_id,
+        NULL AS trigger_event_id,
+        NULL AS parent_action_id,
+        to_timestamp(sent_at::STRING) AS converted_at_utc,
+        {{ pacific_timestamp('converted_at_utc') }} AS converted_at_pt,
+        IFF(journey_id IS NULL, 'broadcast', 'journey') AS email_type,
+        CASE
+            WHEN campaign_id IS NOT NULL
+                THEN 'campaign-' || campaign_id || '-action-' || action_id
+            ELSE
+                'broadcast-' || newsletter_id
+            END AS email_id
+    FROM raw_email_conversions_deprecated
+),
+
+combined_email_conversions AS (
+
+    SELECT
+        *
+    FROM decorated_email_conversions
+    UNION ALL
+    SELECT
+        *
+    FROM decorated_email_conversions_deprecated
 )
 
 SELECT
     *,
     COALESCE(campaign_workspace, newsletter_workspace) AS workspace
-FROM decorated_email_conversions
+FROM combined_email_conversions
 LEFT JOIN campaigns USING (campaign_id)
 LEFT JOIN newsletters USING (newsletter_id)
 WHERE (campaign_id IN (SELECT campaign_id FROM campaigns)
