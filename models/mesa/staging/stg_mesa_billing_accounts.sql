1611c2020ef6aa23129d56e82a678c2499fc1fb9
Jeff Lyon <jeffrey.m.lyon@gmail.com>
2023-09-14 16:38:14 -0700

    remove commented code entirely

--- models/mesa/staging/stg_mesa_billing_accounts.sql
+++ models/mesa/staging/stg_mesa_billing_accounts.sql
@@ -10,35 +10,12 @@ final AS (
     SELECT
         shop_subdomain,
         COALESCE(billing:method:name, 'shopify')::STRING AS billing_method_name,
-        -- {{ pacific_timestamp('to_timestamp(billing:overage:bucket_start::INT, 3)') }} AS billing_overage_bucket_start_date_pt,
-        -- {{ pacific_timestamp('to_timestamp(billing:overage:bucket_end::INT, 3)') }} AS billing_overage_bucket_end_date_pt,
-        -- IFF(IS_NULL_VALUE(billing:overage:bypass_until), NULL, billing:overage:bypass_until) AS billing_overage_bypass_until,
         billing:overage:last_count::FLOAT AS billing_overage_last_count,
         billing:plan:days_complete::FLOAT AS days_complete,
         billing:plan:id::STRING AS plan_id,
         billing:plan:percent_complete::FLOAT AS percent_complete,
         billing:plan:percent_used::FLOAT AS percent_used,
 
-        -- {% set plan_start_date -%}
-        --     to_timestamp(
-        --         CASE
-        --             WHEN IS_NULL_VALUE(billing:plan:start) OR billing:plan:start IS NULL
-        --                 THEN billing:plan_start::INT
-        --             ELSE billing:plan:start::INT
-        --         END, 3)
-        -- {%- endset -%}
-        -- {{ pacific_timestamp(plan_start_date) }} AS start_date_pt,
-
-        -- {% set plan_end_date -%}
-        --     to_timestamp(
-        --         CASE
-        --             WHEN IS_NULL_VALUE(billing:plan:end) OR billing:plan:end IS NULL
-        --                 THEN billing:plan_end::INT
-        --             ELSE billing:plan:end::INT
-        --         END, 3)
-        -- {%- endset -%}
-
-        -- {{ pacific_timestamp(plan_end_date) }} AS end_date_pt,
         billing:plan:status::STRING AS status,
         billing:plan:used::FLOAT AS plan_used,
         billing:plan_name::STRING AS plan_name,
