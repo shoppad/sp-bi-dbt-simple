73e10d9317c128f0541b6451604603460229eb04
Jonathan Crawford <mail@joncrawford.com>
2023-10-24 18:06:09 -0500

    Filter out MESA glue apps from another apps_used column

--- models/generic/staging/stg_constellation_users.sql
+++ models/generic/staging/stg_constellation_users.sql
@@ -49,7 +49,19 @@ constellation_users AS (
             COALESCE(apps_{% if app == "infinite_options" %}customizery{% else %}{{ app }}{% endif %}_installedat, current_timestamp()){%- if not loop.last %}, {% endif -%}
         {% endfor %}, COALESCE(apps_mesa_installedat, current_timestamp())) AS first_shoppad_app_installed_at_utc,
         {{ pacific_timestamp('first_shoppad_app_installed_at_utc') }} AS first_shoppad_app_installed_at_pt,
-        apps_mesa_meta_appsused_value AS apps_used
+        apps_mesa_meta_appsused_value AS apps_used,
+        TRIM(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    ',' || apps_used || ',',
+                    ',({%- for app in var('glue_apps') -%}{{ app }}{%- if not loop.last %}|{% endif -%}{% endfor %}),',
+                    ','
+                ),
+                ',,',
+                ','
+            ),
+            ','
+        ) AS apps_used_without_glue
     FROM {{ source('php_segment', 'users') }}
     LEFT JOIN conversion_rates USING (analytics_currency)
     QUALIFY ROW_NUMBER() OVER (PARTITION BY COALESCE(uuid, id) ORDER BY createdat DESC) = 1
