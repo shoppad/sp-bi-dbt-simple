578a75f22c4eac2c712020c192e4f2ebb2db9e5c
Jonathan Crawford <mail@joncrawford.com>
2023-10-25 17:02:01 -0500

    Move to cleaner way to remove items from apps_used array

--- models/generic/staging/stg_constellation_users.sql
+++ models/generic/staging/stg_constellation_users.sql
@@ -50,18 +50,8 @@ constellation_users AS (
         {% endfor %}, COALESCE(apps_mesa_installedat, current_timestamp())) AS first_shoppad_app_installed_at_utc,
         {{ pacific_timestamp('first_shoppad_app_installed_at_utc') }} AS first_shoppad_app_installed_at_pt,
         apps_mesa_meta_appsused_value AS apps_used,
-        TRIM(
-            REGEXP_REPLACE(
-                REGEXP_REPLACE(
-                    ',' || apps_used || ',',
-                    ',({%- for app in var('glue_apps') -%}{{ app }}{%- if not loop.last %}|{% endif -%}{% endfor %})(,|$)',
-                    ','
-                ),
-                ',,',
-                ','
-            ),
-            ','
-        ) AS apps_used_without_glue
+        ARRAY_TO_STRING(ARRAY_EXCEPT(SPLIT(apps_used, ','), {{ var('glue_apps') }}), ',')
+         AS apps_used_without_glue
     FROM {{ source('php_segment', 'users') }}
     LEFT JOIN conversion_rates USING (analytics_currency)
     QUALIFY ROW_NUMBER() OVER (PARTITION BY COALESCE(uuid, id) ORDER BY createdat DESC) = 1
@@ -69,3 +59,4 @@ constellation_users AS (
 
 SELECT *
 FROM constellation_users
+WHERE apps_used IS NOT NULL
