3a47d0bf1b8fba1fe73b090bf301589a6b30b974
Jonathan Crawford <mail@joncrawford.com>
2023-10-26 12:13:46 -0500

    New columns for source-destinations on Shop

--- models/mesa/intermediate/int_first_workflow_keys.sql
+++ models/mesa/intermediate/int_first_workflow_keys.sql
@@ -1,73 +1,85 @@
-WITH workflows AS (
+with
+    workflows as (select * from {{ ref("int_workflows") }}),
 
-    SELECT *
-    FROM {{ ref('int_workflows') }}
+    workflow_steps as (
 
-),
-
-workflow_steps AS (
-
-    SELECT *
-    FROM {{ ref('stg_workflow_steps') }}
+        select * from {{ ref("stg_workflow_steps") }}
     {# WHERE NOT is_deleted #}
+    ),
 
-),
+    first_workflows as (
+        select *
+        from workflows
+        where step_count > 1
+        qualify
+            row_number() over (partition by shop_subdomain order by created_at_pt asc)
+            = 1
+    ),
 
-first_workflows AS (
-    SELECT *
-    FROM workflows
-    WHERE step_count > 1
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY shop_subdomain ORDER BY created_at_pt ASC) = 1
-),
+    first_workflow_first_steps as (
 
-first_workflow_first_steps AS (
+        select
+            shop_subdomain,
+            workflow_id as first_workflow_id,
+            integration_app as first_workflow_trigger_app,
+            step_key as first_workflow_trigger_key,
+            step_name as first_workflow_trigger_name,
+            workflow_step_id as first_workflow_trigger_step_id,
+            title as first_workflow_title,
+            iff(is_deleted, 'DELETED - ' || title, title) as first_workflow_sort_title,
+            app_chain as first_workflow_app_chain,
+            step_chain as first_workflow_step_chain
+        from first_workflows
+        left join workflow_steps using (workflow_id)
+        where workflow_steps.step_type = 'input'
+        qualify
+            row_number() over (
+                partition by workflow_id order by position_in_workflow desc
+            )
+            = 1
+    ),
 
-    SELECT
-        shop_subdomain,
-        workflow_id AS first_workflow_id,
-        integration_app AS first_workflow_trigger_app,
-        step_key AS first_workflow_trigger_key,
-        step_name AS first_workflow_trigger_name,
-        workflow_step_id AS first_workflow_trigger_step_id,
-        title AS first_workflow_title,
-        IFF(is_deleted, 'DELETED - ' || title, title) AS first_workflow_sort_title,
-        app_chain AS first_workflow_app_chain,
-        step_chain AS first_workflow_step_chain
-    FROM first_workflows
-    LEFT JOIN workflow_steps USING (workflow_id)
-    WHERE workflow_steps.step_type = 'input'
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY workflow_id ORDER BY position_in_workflow DESC) = 1
-),
+    first_workflow_last_steps as (
 
-first_workflow_last_steps AS (
+        select
+            workflow_id as first_workflow_id,
+            integration_app as first_workflow_destination_app,
+            step_key as first_workflow_destination_key,
+            step_name as first_workflow_destination_name
+        from workflow_steps
+        where
+            step_type = 'output'
+            and workflow_step_id not in (
+                select first_workflow_trigger_step_id from first_workflow_first_steps
+            )
+        qualify
+            row_number() over (
+                partition by workflow_id order by position_in_workflow desc
+            )
+            = 1
+    ),
 
-    SELECT
-        workflow_id AS first_workflow_id,
-        integration_app AS first_workflow_destination_app,
-        step_key AS first_workflow_destination_key,
-        step_name AS first_workflow_destination_name
-    FROM workflow_steps
-    WHERE
-        step_type = 'output'
-        AND workflow_step_id NOT IN (
-            SELECT first_workflow_trigger_step_id
-            FROM first_workflow_first_steps
-        )
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY workflow_id ORDER BY position_in_workflow DESC) = 1
-),
+    final as (
 
-final AS (
+        select
+            * exclude first_workflow_trigger_step_id,
+            first_workflow_trigger_app
+            || ' - '
+            || first_workflow_destination_app
+            as first_workflow_trigger_destination_app_pair,
+            first_workflow_trigger_key
+            || ' - '
+            || first_workflow_destination_key
+            as first_workflow_trigger_destination_key_pair,
+            first_workflow_trigger_name
+            || ' - '
+            || first_workflow_destination_name
+            as first_workflow_trigger_destination_name_pair
 
-    SELECT
-        * EXCLUDE first_workflow_trigger_step_id,
-        first_workflow_trigger_app || ' - ' || first_workflow_destination_app AS trigger_destination_app_pair,
-        first_workflow_trigger_key || ' - ' || first_workflow_destination_key AS trigger_destination_key_pair,
-        first_workflow_trigger_name || ' - ' || first_workflow_destination_name AS trigger_destination_name_pair
+        from first_workflow_first_steps
+        left join first_workflow_last_steps using (first_workflow_id)
 
-    FROM first_workflow_first_steps
-    LEFT JOIN first_workflow_last_steps USING (first_workflow_id)
+    )
 
-)
-
-SELECT *
-FROM final
+select *
+from final
--- models/mesa/intermediate/int_mesa_shop_funnel_achievements.sql
+++ models/mesa/intermediate/int_mesa_shop_funnel_achievements.sql
@@ -1,51 +1,35 @@
-WITH
-shops AS (
-    SELECT
-        shop_subdomain,
-        first_installed_at_pt
-    FROM {{ ref('int_shops') }}
-),
+with
+    shops as (select shop_subdomain, first_installed_at_pt from {{ ref("int_shops") }}),
 
-funnel_steps AS (
-    SELECT *
-    FROM {{ ref('mesa_funnel_steps') }}
-),
+    funnel_steps as (select * from {{ ref("mesa_funnel_steps") }}),
 
-workflow_achievements AS (
-    SELECT
-        shop_subdomain,
-        achieved_at_pt,
-        action AS key,
-        'workflow_events' AS source
-    FROM {{ ref('int_workflow_event_achievements') }}
+    workflow_achievements as (
+        select
+            shop_subdomain, achieved_at_pt, action as key, 'workflow_events' as source
+        from {{ ref("int_workflow_event_achievements") }}
 
-    UNION ALL
+        union all
 
-    SELECT
-        shop_subdomain,
-        achieved_at_pt,
-        action AS key,
-        'mesa_flow_events' AS source
-    FROM {{ ref('int_mesa_flow_achievements') }}
+        select
+            shop_subdomain, achieved_at_pt, action as key, 'mesa_flow_events' as source
+        from {{ ref("int_mesa_flow_achievements") }}
 
-    UNION ALL
+        union all
 
-    SELECT
-        shop_subdomain,
-        first_installed_at_pt AS achieved_at_pt,
-        'installed_app' AS key,
-        'hardcoded_in_dbt' AS source
-    FROM shops
-),
+        select
+            shop_subdomain,
+            first_installed_at_pt as achieved_at_pt,
+            'installed_app' as key,
+            'hardcoded_in_dbt' as source
+        from shops
+    ),
 
-funnel_achievements AS (
-    SELECT
-        shop_subdomain,
-        funnel_steps.*,
-        achieved_at_pt
-    FROM funnel_steps
-    LEFT JOIN workflow_achievements USING (key, source)
-    INNER JOIN shops USING (shop_subdomain)
-)
+    funnel_achievements as (
+        select shop_subdomain, funnel_steps.*, achieved_at_pt
+        from funnel_steps
+        left join workflow_achievements using (key, source)
+        inner join shops using (shop_subdomain)
+    )
 
-SELECT * FROM funnel_achievements
+select *
+from funnel_achievements
