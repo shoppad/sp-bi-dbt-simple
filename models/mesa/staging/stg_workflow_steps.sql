b1120da7d670ddf16aaabfe36dc3a4602986c1cb
Jonathan Crawford <mail@joncrawford.com>
2024-01-31 19:41:08 -0600

    Unify step names when missing

--- models/mesa/staging/stg_workflow_steps.sql
+++ models/mesa/staging/stg_workflow_steps.sql
@@ -28,8 +28,8 @@ final AS (
         {{ pacific_timestamp('_created_at') }} AS created_at_pt,
         NULLIF(metadata:description::STRING, '') AS description,
         IFF(is_deleted, 'DELETED - ' || workflow_steps.key, workflow_steps.key) AS step_key,
-        workflow_steps.operation_id,
-        CONCAT(integration_app::STRING || ' - ' || COALESCE(operation_id, integration_app::STRING)::STRING) AS step_name,
+        COALESCE(workflow_steps.operation_id, NULLIF(entity || '_' || action, '_')) AS operation_id,
+        CONCAT(integration_app::STRING || ' - ' || COALESCE(operation_id, NULLIF(entity || '_' || action, '_'), integration_app::STRING)::STRING) AS step_name,
         REPLACE(REPLACE(IFF(is_deleted, 'DELETED - ' || trigger_name, trigger_name), 'In: ', ''), 'Out: ', '') AS step_custom_name,
         ROW_NUMBER() OVER (PARTITION BY workflow_id, step_type ORDER BY step_weight) as position_in_workflow,
         integration_app IN ('{{ var("pro_apps") | join("', '") }}') AS is_pro_app
@@ -37,5 +37,20 @@ final AS (
     LEFT JOIN workflows USING (workflow_id)
 )
 
-SELECT *
+SELECT
+    *
+    {# Removes variation in operation_ids #}
+    REPLACE
+    (CASE WHEN operation_id LIKE 'order_%' THEN REPLACE(operation_id, 'order_', 'orders_')
+    ELSE operation_id
+    END AS operation_id,
+    CASE
+        WHEN operation_id LIKE 'order_%' THEN REPLACE(step_name, 'order_', 'orders_')
+        ELSE step_name
+    END AS step_name
+    )
 FROM final
+{# WHERE
+ (workflow_id = '60e4a1f6ee72cb0ba93fa5a4'
+ OR workflow_id = '61de013cf0259f354243b582')
+ AND step_type = 'input' #}
