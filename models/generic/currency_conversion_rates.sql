2cd27f54977f54b8223a8161a2b7047572be1daa
Jonathan Crawford <mail@joncrawford.com>
2024-03-31 11:46:02 -0500

    Only return the latest conversion rate for each currency.

--- models/generic/currency_conversion_rates.sql
+++ models/generic/currency_conversion_rates.sql
@@ -1,11 +1,19 @@
-SELECT
-    quote_currency_id AS currency,
-    value AS in_usd,
-    date AS exchange_rate_at
-FROM {{ source('economy_data', 'currency_conversion_rates') }}
-WHERE base_currency_id = 'USD'
-{# QUALIFY ROW_NUMBER() OVER (PARTITION BY "Currency Name" ORDER BY "Date" DESC) = 1 #}
+WITH
+latest_current_rates_in_usd AS (
+    SELECT
+        quote_currency_id AS currency,
+        MAX_BY(value, date) AS in_usd,
+        MAX(date) AS exchange_rate_at
+    FROM {{ source('economy_data', 'currency_conversion_rates') }}
+    WHERE base_currency_id = 'USD'
+    GROUP BY 1
+)
+
+SELECT *
+FROM latest_current_rates_in_usd
+
 UNION ALL
+
 SELECT
     'USD' AS currency,
     1 AS in_usd,
