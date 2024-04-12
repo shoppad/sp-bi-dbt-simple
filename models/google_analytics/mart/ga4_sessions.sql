ccfec41bd781f63b478519a44839fab24a642103
Jonathan Crawford <mail@joncrawford.com>
2024-04-12 12:27:31 -0500

    Add session referrer to ga4 session model

--- models/google_analytics/mart/ga4_sessions.sql
+++ models/google_analytics/mart/ga4_sessions.sql
@@ -1,5 +1,12 @@
 WITH
-ga4_events AS (
+session_starts AS (
+    SELECT
+        *
+    FROM {{ ref('int_ga4_events') }}
+    WHERE event_name = 'session_start'
+),
+
+pageview_events AS (
     SELECT
         *
     FROM {{ ref("int_ga4_events") }}
@@ -12,7 +19,7 @@ page_counts AS (
          ga_session_number,
          MAX(shop_subdomain) AS shop_subdomain,
          COUNT(*) AS page_view_count
-    FROM ga4_events
+    FROM pageview_events
     GROUP BY 1, 2
 ),
 
@@ -31,11 +38,15 @@ landing_pages AS (
             page_location_path AS landing_page_path,
             page_location_url AS landing_page_url,
             page_location_query AS landing_page_query,
-            page_location_host AS landing_page_host
+            page_location_host AS landing_page_host,
+            page_referrer_host AS session_referrer_host,
+            page_referrer_url AS session_referrer_url,
+            page_referrer_path AS session_referrer_path,
+            page_referrer_query AS session_referrer_query,
+            page_referrer_full AS session_referrer_full
         )
 
-    FROM ga4_events
-    WHERE is_landing_pageview
+    FROM session_starts
 ),
 
 exit_pages AS (
@@ -48,14 +59,20 @@ exit_pages AS (
         page_location_url AS exit_page_url,
         page_location_query AS exit_page_search,
         page_location_host AS exit_page_host
-    FROM ga4_events
+    FROM pageview_events
     WHERE is_exit_pageview
+),
+
+final AS (
+
+    SELECT
+        *
+    FROM landing_pages
+    LEFT JOIN page_counts
+        USING (ga_session_id)
+    LEFT JOIN exit_pages
+        USING (ga_session_id)
 )
 
-SELECT
-    *
-FROM page_counts
-LEFT JOIN landing_pages
-    USING (ga_session_id)
-LEFT JOIN exit_pages
-    USING (ga_session_id)
+SELECT *
+FROM final
